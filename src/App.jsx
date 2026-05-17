d61203a209dd1e4ba6fc24a98a745157f3c51d9f
imsunxinhao <imsunxinhao@163.com>
2026-05-17 22:15:08 +0800

    fix: 移动能力变量  On branch fix-ability-move  Changes to be committed: 	modified:   src/App.jsx 	modified:   src/locales/index.js

--- src/App.jsx
+++ src/App.jsx
@@ -57,48 +57,7 @@ const PHOTO_Y = 160;
 const PHOTO_WIDTH = 220;
 const PHOTO_HEIGHT = 275;
 
-const ability_options = [
-  {
-    value: '御灵系',
-    label: '御灵系',
-    children: [
-      { value: '金', label: '金' },
-      { value: '木', label: '木' },
-      { value: '水', label: '水' },
-      { value: '火', label: '火' },
-      { value: '土', label: '土' },
-    ],
-  },
-  {
-    value: '空间系',
-    label: '空间系',
-    children: [
-      { value: '领域', label: '领域' },
-      { value: '传送', label: '传送' },
-      { value: '吞噬', label: '吞噬' },
-    ],
-  },
-  {
-    value: '造物系',
-    label: '造物系'
-  },
-  {
-    value: '生灵系',
-    label: '生灵系'
-  },
-  {
-    value: '心灵系',
-    label: '心灵系'
-  },
-  {
-    value: '乱七八糟系',
-    label: '乱七八糟系'
-  },
-  {
-    value: '',
-    label: '其他'
-  }
-];
+
 
 
 function App() {
